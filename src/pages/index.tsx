e90a5333ed42e4f151fec48ee4d7c1e090f912f3
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-19 22:39:41 +0900

    npm init
    query / redux / bootstrap

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>나의 영화 일지</title>
+        <meta name="description" content="영화 일기장" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
