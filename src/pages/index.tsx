0a955af580a647841aec1c5d2c662ad551962ab7
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-21 23:43:22 +0900

    feat: theme settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
+import Header from "@/components/common/Header";
+import { Button, useColorScheme } from "@mui/material";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // to avoid layout shift, render a placeholder button
+    return <Button variant="outlined" color="error" sx={{ width: 120 }} />;
+  }
+
   return (
     <>
       <Head>
@@ -9,6 +24,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>
+        {mode === "dark" ? "Turn light" : "Turn dark"}
+      </Button>
+      <Header></Header>
     </>
   );
 }
