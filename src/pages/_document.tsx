62b3936c66e7cc964d575cec5025511571fba593
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-21 23:43:09 +0900

    mui theme settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material";
+import { useMemo, useState } from "react";
+import { customtheme } from "@/styles/theme";
+import { useColorMode } from "@/hooks/useColorMode";
 
 export default function Document() {
+  const { mode } = useColorMode();
+  const theme = useMemo(() => customtheme(mode), [mode]);
+
   return (
-    <Html lang="en">
+    <Html lang="en" data-color-scheme="light">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <Main />
+            <NextScript />
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </Html>
-  )
+  );
 }
