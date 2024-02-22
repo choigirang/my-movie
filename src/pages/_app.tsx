ac666eb0295a05707a83ab5a0cc4117c14d19f78
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-22 19:58:22 +0900

    feat: add styled engine & AppRouterProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import { ColorModeProvider, useThemeHandler } from "@/util/useThemeHandler";
 import "@/styles/globals.css";
+import { createCustomTheme } from "@/styles/theme";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StyledEngineProvider injectFirst>
+      <AppRouterCacheProvider options={{ enableCssLayer: true }} {...pageProps}>
+        <ColorModeProvider>
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </AppRouterCacheProvider>
+    </StyledEngineProvider>
+  );
 }
