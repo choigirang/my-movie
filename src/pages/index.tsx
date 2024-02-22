ffeb9f480cae85d03a19ff98a52e8cad5c0fb709
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-22 19:58:40 +0900

    feat: add Container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,10 @@
 import Header from "@/components/common/Header";
-import { Button, useColorScheme } from "@mui/material";
+import { useThemeHandler } from "@/util/useThemeHandler";
+import { Button, Container, useColorScheme } from "@mui/material";
 import Head from "next/head";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { mode, setMode } = useColorScheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    // to avoid layout shift, render a placeholder button
-    return <Button variant="outlined" color="error" sx={{ width: 120 }} />;
-  }
+  const { toggleColorMode } = useThemeHandler();
 
   return (
     <>
@@ -24,10 +14,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Button onClick={() => setMode(mode === "dark" ? "light" : "dark")}>
-        {mode === "dark" ? "Turn light" : "Turn dark"}
-      </Button>
-      <Header></Header>
+      {/* <Button onClick={toggleColorMode}></Button> */}
+      <Container
+        sx={{ width: "100%", display: "flex", justifyContent: "center" }}
+      >
+        <Header></Header>
+      </Container>
     </>
   );
 }
