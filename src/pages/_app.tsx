f9818337e469926c374bd33ed9a672f5b1ad054f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-24 23:41:22 +0900

    feat: style cache issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,32 @@
-import { ColorModeProvider, theme, useColorMode } from "@/util/useColorMode";
+import { ColorModeProvider, useColorMode } from "@/hook/useColorMode";
 import "@/styles/globals.css";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { useMemo } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { createCustomTheme } from "@/styles/theme";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StyledEngineProvider injectFirst>
-      <AppRouterCacheProvider options={{ enableCssLayer: true }} {...pageProps}>
+    <QueryClientProvider client={queryClient}>
+      <StyledEngineProvider injectFirst>
         {/* <ColorModeProvider> */}
-          {/* <ThemeProvider theme={theme}> */}
-            <Component {...pageProps} />
-          {/* </ThemeProvider> */}
+        <ThemeProvider theme={createCustomTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
         {/* </ColorModeProvider> */}
-      </AppRouterCacheProvider>
-    </StyledEngineProvider>
+      </StyledEngineProvider>
+    </QueryClientProvider>
   );
 }
