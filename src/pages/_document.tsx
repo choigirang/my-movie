7ab33880f78e5251f524b1e4a3b406fa6bc1f346
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-24 23:41:41 +0900

    feat: ssr mui style 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,34 @@
-import { Html, Head, Main, NextScript, DocumentProps } from "next/document";
+import {
+  Head,
+  Main,
+  NextScript,
+  DocumentProps,
+  DocumentContext,
+} from "next/document";
 import {
   DocumentHeadTags,
   DocumentHeadTagsProps,
+  documentGetInitialProps,
 } from "@mui/material-nextjs/v13-pagesRouter";
 import Header from "@/components/common/Header";
+import { NextPageContext } from "next";
 
 export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
-    <Html lang="en" data-color-scheme="light">
+    <>
       <Head>
         <DocumentHeadTags {...props} />
       </Head>
       <body>
+        <Header />
         <Main />
         <NextScript />
-        <Header></Header>
       </body>
-    </Html>
+    </>
   );
 }
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
