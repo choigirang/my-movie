8ebc3f13c64563916136f2754bbc5802db2aacb5
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-25 23:35:02 +0900

    feat: add next images domain

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["image.tmdb.org"], // 사용하는 이미지 호스트를 여기에 추가
+  },
 };
 
 module.exports = nextConfig;
