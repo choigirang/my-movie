4aaf6684d59e24cf94084a38f672c508ab5651c1
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-25 23:36:01 +0900

    feat: useInfiniteQuery를 통한 무한 스크롤

--- src/components/home/MovieList.tsx
+++ src/components/home/MovieList.tsx
@@ -1,15 +1,50 @@
 import useMovieData from "@/hook/useMovieData";
 import { Container, Grid } from "@mui/material";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MovieInfo from "./MovieInfo";
 import { MovieDetailType } from "@/type/movie";
 
 export default function MovieList() {
-  const { data } = useMovieData({ page: undefined });
+  const target = useRef<HTMLDivElement>(null);
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useMovieData();
+
+  useEffect(() => {
+    if (target.current && !hasNextPage) {
+      return;
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 0.5 }
+    );
+    if (target.current) {
+      observer.observe(target.current);
+    }
+
+    return () => {
+      if (target.current) {
+        observer.unobserve(target.current);
+      }
+    };
+  }, [target.current, data?.pageParams]);
 
   return (
     <Grid container spacing={4}>
-      {data && data.map((each) => <MovieInfo key={each.id} {...each} />)}
+      {data &&
+        data.pages.map((page, pageIndex) => (
+          <React.Fragment key={pageIndex}>
+            {page.map((movie: MovieDetailType) => (
+              <MovieInfo key={movie.id} {...movie} />
+            ))}
+          </React.Fragment>
+        ))}
+      {isFetchingNextPage && <div>Loading...</div>}
+      {!hasNextPage && <div>End of List</div>}
+      <div ref={target} />
     </Grid>
   );
 }
