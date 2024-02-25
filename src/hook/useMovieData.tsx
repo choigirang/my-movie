a28b55ecdb98ca8bc073748211a201af413b7a79
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-25 23:36:13 +0900

    feat: useQuery => useInfiniteQuery

--- src/hook/useMovieData.tsx
+++ src/hook/useMovieData.tsx
@@ -1,21 +1,21 @@
 import { api } from "@/api/api";
 import { MovieDetailType } from "@/type/movie";
 import React from "react";
-import { useQuery } from "react-query";
+import { useInfiniteQuery, useQuery } from "react-query";
 
-export default function useMovieData({
-  page,
-  keywords,
-}: {
-  page?: number;
-  keywords?: string;
-}) {
-  async function fetchMove() {
-    const res: MovieDetailType[] = (await api.get(``)).data.results;
-    return res;
+export default function useMovieData() {
+  async function fetchMovies({ pageParam = 1 }: { pageParam?: number }) {
+    const res = await api.get(``, {
+      params: {
+        page: pageParam,
+      },
+    });
+    return res.data.results;
   }
 
-  return useQuery(["movie"], fetchMove, {
-    staleTime: 2000,
+  return useInfiniteQuery("movies", fetchMovies, {
+    getNextPageParam: (lastPage, allPages) => {
+      return allPages.length + 1;
+    },
   });
 }
