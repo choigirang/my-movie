e12f8af57694ba592f6514dc2fcf5f0adf310e7d
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-26 21:23:38 +0900

    feat: add data detail pop up

--- src/components/home/MovieList.tsx
+++ src/components/home/MovieList.tsx
@@ -1,14 +1,19 @@
 import useMovieData from "@/hook/useMovieData";
 import { Container, Grid } from "@mui/material";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MovieInfo from "./MovieInfo";
 import { MovieDetailType } from "@/type/movie";
+import { PropagateLoader } from "react-spinners";
+import MovieDetail from "./MovieDetail";
 
 export default function MovieList() {
+  const [detailData, setDetailData] = useState<MovieDetailType | undefined>();
   const target = useRef<HTMLDivElement>(null);
+  // useInfiniteQuery hooks
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useMovieData();
 
+  // current observer
   useEffect(() => {
     if (target.current && !hasNextPage) {
       return;
@@ -19,7 +24,7 @@ export default function MovieList() {
           fetchNextPage();
         }
       },
-      { threshold: 0.5 }
+      { threshold: 1 }
     );
     if (target.current) {
       observer.observe(target.current);
@@ -34,16 +39,33 @@ export default function MovieList() {
 
   return (
     <Grid container spacing={4}>
+      {/* 영화 상세 정보 팝업 */}
+      {detailData && (
+        <MovieDetail
+          {...detailData}
+          setDetailData={setDetailData}
+        ></MovieDetail>
+      )}
       {data &&
         data.pages.map((page, pageIndex) => (
           <React.Fragment key={pageIndex}>
             {page.map((movie: MovieDetailType) => (
-              <MovieInfo key={movie.id} {...movie} />
+              <MovieInfo
+                key={movie.id}
+                {...movie}
+                setDetailData={setDetailData}
+              />
             ))}
           </React.Fragment>
         ))}
-      {isFetchingNextPage && <div>Loading...</div>}
+      {/* 스피너 */}
+      <PropagateLoader
+        loading={isFetchingNextPage}
+        size={150}
+        cssOverride={{ display: "flex", margin: "0 auto" }}
+      />
       {!hasNextPage && <div>End of List</div>}
+      {/* overrides */}
       <div ref={target} />
     </Grid>
   );
