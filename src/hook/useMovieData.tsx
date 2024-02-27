c4e902383fbbfe25532f9946b10857d8aa124130
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-27 23:42:39 +0900

    feat: add search url in infiniteQuery

--- src/hook/useMovieData.tsx
+++ src/hook/useMovieData.tsx
@@ -1,15 +1,18 @@
 import { api } from "@/api/api";
 import { MovieDetailType } from "@/type/movie";
 import React from "react";
-import { useInfiniteQuery, useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 
-export default function useMovieData() {
+export default function useMovieData(keyword?: string | number) {
   async function fetchMovies({ pageParam = 1 }: { pageParam?: number }) {
-    const res = await api.get(``, {
-      params: {
-        page: pageParam,
-      },
-    });
+    const res = await api.get(
+      `${keyword !== "" ? "search/movie" : "movie/popular"}`,
+      {
+        params: {
+          page: pageParam,
+        },
+      }
+    );
     return res.data.results;
   }
 
