0895497f63d23d2e53ad7059bf5715c0067f38c5
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-27 23:43:22 +0900

    feat: add pop up components props

--- src/type/common.ts
+++ src/type/common.ts
@@ -1,4 +1,12 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+
 export type ColorModeContextType = {
   mode: "light" | "dark";
   toggleColorMode: () => void;
 };
+
+// 공통 Alert 컴포넌트 props
+export interface AlertProps {
+  children?: ReactNode;
+  setState: Dispatch<SetStateAction<any>>;
+}
