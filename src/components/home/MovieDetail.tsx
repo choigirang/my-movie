7d349edb431d5ebc0a9d9df0ed9bb6fed8ec0e95
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-27 23:41:42 +0900

    feat: AlertBg로 공통 컴포넌트 병합

--- src/components/home/MovieDetail.tsx
+++ src/components/home/MovieDetail.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { ClipLoader, MoonLoader, PropagateLoader } from "react-spinners";
 import DetailDes from "./DetailDes";
 import ClearIcon from "@mui/icons-material/Clear";
+import AlertBg from "../common/AlertBg";
+import Alert from "../common/Alert";
 
 export default function MovieDetail(movieDetail: MovieInfoProps) {
   const [loading, setLoading] = useState(true); // 이미지 로딩 상태
@@ -18,13 +20,8 @@ export default function MovieDetail(movieDetail: MovieInfoProps) {
   };
 
   return (
-    <Bg onClick={() => movieDetail.setDetailData(undefined)}>
-      <GridCustom
-        container
-        spacing={1200 <= window.innerWidth ? 2 : 1}
-        columnSpacing={1200 <= window.innerWidth ? 1 : 2}
-        onClick={(e) => e.stopPropagation()}
-      >
+    <AlertBg setState={movieDetail.setDetailData}>
+      <Alert setState={movieDetail.setDetailData}>
         {/* 이미지 */}
         <ImageBox item xs>
           {loading && (
@@ -43,50 +40,10 @@ export default function MovieDetail(movieDetail: MovieInfoProps) {
         </ImageBox>
         {/* 영화 상세 설명 */}
         <DetailDes {...movieDetail} />
-        {/* 닫기 버튼 */}
-        <CloseBtn
-          startIcon={<ClearIcon />}
-          onClick={() => movieDetail.setDetailData(undefined)}
-        />
-      </GridCustom>
-    </Bg>
+      </Alert>
+    </AlertBg>
   );
 }
-
-const Bg = MuiStyled("div")(({}) => ({
-  width: "100%",
-  height: "100%",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  position: "fixed",
-  top: 0,
-  left: 0,
-  backgroundColor: "rgba(0,0,0,0.5)",
-  zIndex: 999,
-}));
-
-const GridCustom = MuiStyled(Grid)({
-  flexDirection: "row",
-  width: "70%",
-  height: "90%",
-  display: "flex",
-  backgroundColor: "rgb(24, 24, 24)",
-  borderRadius: "10px",
-  position: "relative",
-
-  /* 타블렛 */
-  "@media screen and (min-width: 768px) and (max-width: 1200px)": {
-    flexDirection: "column",
-    overflow: "scroll",
-    gap: 20,
-    padding: "20px 0",
-    "& > * + *": {
-      // 첫 번째 자식 이후의 모든 자식에 대해
-      marginTop: 0, // 상단 간격을 없애줌
-    },
-  },
-});
 
 const ImageBox = MuiStyled(Grid)({
   position: "relative",
@@ -104,17 +61,4 @@ const Spinner = MuiStyled("div")({
   left: "50%",
   top: "50%",
   transform: "translate(-50%,-50%)",
-});
-
-const CloseBtn = MuiStyled(Button)({
-  position: "absolute",
-  top: 0,
-  right: 0,
-  width: 24,
-  height: 30,
-  backgroundColor: "rgba(0,0,0,0)",
-
-  span: {
-    margin: 0,
-  },
 });
