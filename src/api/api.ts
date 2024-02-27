81d1ce534792d41b128881e9ba1423ed2696b2e2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-27 23:38:36 +0900

    feat: add api interceptors

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ const url = process.env.NEXT_PUBLIC_MOVIE_URL;
 const key = process.env.NEXT_PUBLIC_MOVIE_KEY;
 
 export const api = axios.create({
-  baseURL: `${url}?api_key=${key}&language=ko-KR&region=kr`,
+  baseURL: `${url}`,
   // timeout: 5000,
   headers: {
     "Content-Type": "application/json",
@@ -12,4 +12,15 @@ export const api = axios.create({
   // withCredentials: true,
 });
 
-// api.interceptors.request.use();
+api.interceptors.request.use((config) => {
+  const { params } = config;
+
+  config.params = {
+    ...params,
+    api_key: key,
+    language: "ko-KR",
+    region: "kr",
+  };
+
+  return config;
+});
