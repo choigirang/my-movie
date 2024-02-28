f93602ad2a79e911ca66af435b9b31f3e475fe1b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-28 23:43:51 +0900

    feat: next RTK settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,10 @@ import "@/styles/globals.css";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import { useMemo } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { createCustomTheme } from "@/styles/theme";
+import wrapper from "@/store";
+import PropTypes from "prop-types";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,7 +18,7 @@ export const queryClient = new QueryClient({
   },
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   const theme = createCustomTheme();
 
   return (
@@ -34,3 +35,9 @@ export default function App({ Component, pageProps }: AppProps) {
     </QueryClientProvider>
   );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default wrapper.withRedux(App);
