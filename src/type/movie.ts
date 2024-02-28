570fab8720698be173b89d3d8f836279a8014c8b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-28 23:45:05 +0900

    feat: 무비 타입 분리

--- src/type/movie.ts
+++ src/type/movie.ts
@@ -1,22 +1,23 @@
 import { Dispatch, RefObject, SetStateAction } from "react";
-import { InfiniteData } from "react-query";
+import {
+  FetchNextPageOptions,
+  InfiniteData,
+  InfiniteQueryObserverResult,
+} from "react-query";
 
-export type MovieDetailType = {
+export interface MovieDetailType extends SelectMovieData {
   adult: boolean;
   backdrop_path: string;
-  genre_ids: Array<number>;
-  id: number;
   original_language: string;
   original_title: string;
   overview: string;
   popularity: number;
   poster_path: string;
   release_date: string;
-  title: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
-};
+}
 
 export interface MovieInfoProps extends MovieDetailType {
   setDetailData: Dispatch<SetStateAction<undefined | MovieDetailType>>;
@@ -25,6 +26,16 @@ export interface MovieInfoProps extends MovieDetailType {
 export type SearchResultsProps = {
   keyword: string | number;
   apiKeyword: string | number;
-  observe: RefObject<HTMLLIElement>;
-  data: InfiniteData<MovieDetailType> | undefined;
+  data: InfiniteData<MovieDetailType[]> | undefined;
+  hasNextPage: boolean | undefined;
+  fetchNextPage: (
+    options?: FetchNextPageOptions | undefined
+  ) => Promise<InfiniteQueryObserverResult<any, unknown>>;
+  isFetchingNextPage: boolean;
 };
+
+export interface SelectMovieData {
+  id: number | undefined;
+  title: string | undefined;
+  genre_ids: Array<number> | undefined;
+}
