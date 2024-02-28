25c8d714956b8f921c3fc75a932e7fb53ff2fd26
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-28 23:42:48 +0900

    feat: ref mapping => data mapping

--- src/components/calendar/SearchResults.tsx
+++ src/components/calendar/SearchResults.tsx
@@ -8,43 +8,71 @@ import React, {
 } from "react";
 import { InfiniteData } from "react-query";
 import { styled as MuiStyled } from "@mui/material";
+import { PropagateLoader } from "react-spinners";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 export default function SearchResults(props: SearchResultsProps) {
-  const [prevEls, setPrevEls] = useState<ReactNode[]>([]);
-  const prevEl = useRef<HTMLLIElement>(null);
-  const { keyword, apiKeyword, observe, data } = props;
+  const dispatch = useDispatch();
+  const selectMovie = useSelector((state: RootState) => state);
 
-  useEffect(() => {
-    if (keyword !== apiKeyword) {
-      prevEl.current && prevEl.current.remove();
-    }
-
-    // data가 변경될 때마다 PrevData 요소들을 추적하여 상태 업데이트
-    if (data) {
-    }
-  }, [data, keyword, apiKeyword]);
   return (
-    <DataBox>
-      {...prevEls}
-      {/* <PrevData ref={observe}>더 보기</PrevData> */}
-    </DataBox>
+    <React.Fragment>
+      {/* 검색 중일 시 숨기기 */}
+      {props.keyword === props.apiKeyword && (
+        <DataBox>
+          {/* 데이터 매핑 */}
+          {props.data &&
+            props.data.pages.map((page) =>
+              page.map((movie) => (
+                <PrevData key={movie.id}>{movie.title}</PrevData>
+              ))
+            )}
+          {props.keyword === "" && <PrevData>검색어를 입력해주세요.</PrevData>}
+          {/* 스피너 */}
+          {props.isFetchingNextPage && (
+            <PropagateLoader
+              loading={props.isFetchingNextPage}
+              size={100}
+              cssOverride={{ display: "flex", margin: "0 auto" }}
+            />
+          )}
+          {/* 더 보기 있을 시 */}
+          {props.hasNextPage && (
+            <PrevData onClick={() => props.fetchNextPage()}>
+              ...더 보기
+            </PrevData>
+          )}
+        </DataBox>
+      )}
+    </React.Fragment>
   );
 }
 
 const DataBox = MuiStyled("ul")({
-  width: "inherit",
-  height: 290,
+  width: "calc(100% - 10px)",
+  maxHeight: 280,
   position: "absolute",
-  left: 0,
+  left: "50%",
   top: 50,
+  transform: "translateX(-50%)",
   display: "flex",
   flexDirection: "column",
   backgroundColor: "white",
   overflow: "scroll",
   gap: 10,
+  borderBottomLeftRadius: "5px 5px",
+  borderBottomRightRadius: "5px 5px",
 });
 
 const PrevData = MuiStyled("li")({
   width: "100%",
-  height: 10,
+  padding: 10,
+  fontSize: 12,
+
+  "&:hover": {
+    backgroundColor: "#b8b8b8",
+    cursor: "pointer",
+  },
 });
