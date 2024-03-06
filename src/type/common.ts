12e46ceb6463579f9c47413c724d11536202779b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-06 23:59:23 +0900

    feat: 공통 컴포넌트에서 사용될 customStyle

--- src/type/common.ts
+++ src/type/common.ts
@@ -5,8 +5,13 @@ export type ColorModeContextType = {
   toggleColorMode: () => void;
 };
 
+export interface CustomStyle {
+  [key: string]: string | number;
+}
+
 // 공통 Alert 컴포넌트 props
 export interface AlertProps {
   children?: ReactNode;
   setState: Dispatch<SetStateAction<any>>;
+  customStyle?: CustomStyle;
 }
