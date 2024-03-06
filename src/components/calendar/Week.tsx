15ae60f616cd7b8f13e833892bdae8c3c8305d2b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-06 23:51:50 +0900

    feat: 캘린더 mapping 데이터 변경으로 인한 타입 변경
    기존 : [0, 0, 0, 0, 1, 2, 3]
    => [0, 0, 0, 0, {date: "2024-3-1", movies: [...]}, ...]

--- src/components/calendar/Week.tsx
+++ src/components/calendar/Week.tsx
@@ -1,3 +1,5 @@
+import { useAppSelector } from "@/hook/useRedux";
+import { SaveMovieInfo } from "@/type/movie";
 import { Container, styled as MuiStyled } from "@mui/material";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 
@@ -5,16 +7,31 @@ export default function Week({
   days,
   clickDay,
 }: {
-  days: Array<number>;
+  days: (0 | { date: string; movies: Array<SaveMovieInfo> })[];
   clickDay: Dispatch<SetStateAction<boolean>>;
 }) {
-  const [curDate, setCurData] = useState(new Date().getDay());
+  const [curDate, setCurData] = useState(new Date().getDate());
 
-  function satOrSunDay(num: number) {
-    if (num === days[0]) return "#ff3d3d";
-    else if (num === days[days.length - 1]) return "#3d77ff";
+  const satOrSunDay = (num: number) => {
+    if (num === 0) return "#ff3d3d";
+    else if (num === days.length - 1) return "#3d77ff";
     else return "white";
-  }
+  };
+
+  const existDay = (existDay: boolean) => {
+    if (!existDay) return false;
+    else {
+      clickDay(true);
+      return true;
+    }
+  };
+
+  const addMovie = (movies: SaveMovieInfo) => {
+    const { title, genre_ids, vote_average } = movies;
+    console.log("movies", movies);
+
+    return <div>{title}</div>;
+  };
 
   if (!days) return <div>조회된 데이터가 없습니다.</div>;
 
@@ -23,11 +40,13 @@ export default function Week({
       {days.map((day, index) => (
         <Days
           key={index}
-          changeColor={satOrSunDay(day)}
+          changeColor={satOrSunDay(index)}
+          trashValue={day === 0}
           today={curDate === day}
-          onClick={() => clickDay(true)}
+          onClick={() => existDay(day !== 0)}
         >
-          {day !== 0 && day}
+          <span>{day !== 0 && new Date(day.date).getDate()}</span>
+          {day !== 0 && day.movies.map((info) => addMovie(info))}
         </Days>
       ))}
     </Wrapper>
@@ -40,19 +59,24 @@ const Wrapper = MuiStyled(Container)({
   gridTemplateColumns: "repeat(7, 1fr)",
 });
 
-const Days = MuiStyled("div")<{ changeColor: string; today: boolean }>(
-  ({ changeColor, today }) => ({
-    width: "100%",
-    height: 100,
-    padding: 10,
-    borderRadius: 5,
-    color: changeColor,
-    backgroundColor: today ? "#344340ff" : "none",
+const Days = MuiStyled("div")<{
+  changeColor: string;
+  today: boolean;
+  trashValue: boolean;
+}>(({ changeColor, today, trashValue }) => ({
+  width: "100%",
+  height: 100,
+  padding: 10,
+  borderRadius: 5,
+  color: changeColor,
+  backgroundColor: today ? "#344340ff" : "none",
+  overflow: "scroll",
 
+  ...(!trashValue && {
     "&:hover": {
       transition: "all .3s",
       backgroundColor: "#526965",
       cursor: "pointer",
     },
-  })
-);
+  }),
+}));
