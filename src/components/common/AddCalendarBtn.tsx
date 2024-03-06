1e26fae7891db43716e7b19976c901a4548acb90
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-07 00:50:50 +0900

    feat: input date 형식 맞춤, savedMovie "2023-3-4"

--- src/components/common/AddCalendarBtn.tsx
+++ src/components/common/AddCalendarBtn.tsx
@@ -6,24 +6,33 @@ import { useAppDispatch, useAppSelector } from "@/hook/useRedux";
 import { resetSelect } from "@/store/modules/movieSelectSlice";
 import { savedMovie } from "@/store/modules/savedMovieSlice";
 
-export default function AddCalendarBtn(movieDetail: MovieInfoProps) {
+export default function AddCalendarBtn() {
   const [date, setDate] = useState("");
   const dispatch = useAppDispatch();
   const select = useAppSelector((state) => state.movieSlice);
 
   const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setDate(e.target.value);
+    const splitingDate = e.target.value.split("-");
+    const trimmedDate = splitingDate.map((str) => {
+      if (str.startsWith("0")) {
+        return str.substr(1);
+      }
+      return str;
+    });
+    const deleteZeroDate = trimmedDate.join("-");
+    setDate(deleteZeroDate);
   };
 
   const dispatchHandler = () => {
     if (!date || !select) return alert("영화 정보와 날짜를 입력해주세요.");
-    dispatch(savedMovie({ ...movieDetail, date }));
+    dispatch(savedMovie({ ...select, date }));
     dispatch(resetSelect());
   };
 
   return (
     <Container>
-      <input type="date" onChange={handleDateChange} />
+      {/* !클릭한 날짜 전달하기  */}
+      <input type="date" onChange={handleDateChange} value={"2024-05-03"} />
       <CustomBtn
         startIcon={<EditCalendarIcon />}
         variant="contained"
