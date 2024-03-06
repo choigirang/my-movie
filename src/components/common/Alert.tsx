c7303c7c7cb51030e332002db85d05b486d66b3f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-06 23:53:16 +0900

    feat: X 버튼 이벤트 추가 및 스타일 커스텀 시 사용할 props추가

--- src/components/common/Alert.tsx
+++ src/components/common/Alert.tsx
@@ -1,44 +1,63 @@
 import React, { Dispatch, ReactNode, SetStateAction } from "react";
 import { Grid, Button, styled as MuiStyled } from "@mui/material";
-import { AlertProps } from "@/type/common";
+import { AlertProps, CustomStyle } from "@/type/common";
 import ClearIcon from "@mui/icons-material/Clear";
+import { useAppDispatch } from "@/hook/useRedux";
+import { resetSelect } from "@/store/modules/movieSelectSlice";
 
-export default function Alert({ children, setState }: AlertProps) {
+export default function Alert({ children, setState, customStyle }: AlertProps) {
+  const dispatch = useAppDispatch();
+
+  const resetAlertWithMovieData = () => {
+    setState(false);
+    dispatch(resetSelect());
+  };
+
   return (
     <GridCustom
       container
       spacing={1200 <= window.innerWidth ? 2 : 1}
       columnSpacing={1200 <= window.innerWidth ? 1 : 2}
       onClick={(e) => e.stopPropagation()}
+      customStyle={customStyle}
     >
       {children}
       {/* 닫기 버튼 */}
-      <CloseBtn startIcon={<ClearIcon />} onClick={() => setState(undefined)} />
+      <CloseBtn
+        startIcon={<ClearIcon />}
+        onClick={() => resetAlertWithMovieData()}
+      />
     </GridCustom>
   );
 }
 
-const GridCustom = MuiStyled(Grid)({
-  flexDirection: "row",
-  width: "70%",
-  height: "90%",
-  display: "flex",
-  backgroundColor: "rgb(24, 24, 24)",
-  borderRadius: "10px",
-  position: "relative",
-  padding: "20px",
+const GridCustom = MuiStyled(Grid)<{ customStyle?: CustomStyle }>(
+  ({ customStyle }) => ({
+    flexDirection: "row",
+    width: "70%",
+    height: "90%",
+    display: "flex",
+    backgroundColor: "rgb(24, 24, 24)",
+    borderRadius: "10px",
+    position: "relative",
+    padding: "20px",
 
-  /* 타블렛 */
-  "@media screen and (min-width: 768px) and (max-width: 1200px)": {
-    flexDirection: "column",
-    overflow: "scroll",
-    gap: 20,
-    "& > * + *": {
-      // 첫 번째 자식 이후의 모든 자식에 대해
-      marginTop: 0, // 상단 간격을 없애줌
+    ...(customStyle && {
+      ...customStyle,
+    }),
+
+    /* 타블렛 */
+    "@media screen and (min-width: 768px) and (max-width: 1200px)": {
+      flexDirection: "column",
+      overflow: "scroll",
+      gap: 20,
+      "& > * + *": {
+        // 첫 번째 자식 이후의 모든 자식에 대해
+        marginTop: 0, // 상단 간격을 없애줌
+      },
     },
-  },
-});
+  })
+);
 
 const CloseBtn = MuiStyled(Button)({
   position: "absolute",
