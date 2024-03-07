1fbf47b1b5cc0fba9d15672fa0e7d0d10c65baba
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-07 22:25:18 +0900

    feat: add delete reducer & 데이터 조건부 저장

--- src/store/modules/savedMovieSlice.ts
+++ src/store/modules/savedMovieSlice.ts
@@ -28,15 +28,32 @@ export const savedMovieSlice = createSlice({
 
       // date 키가 이미 initialState에 존재하는지 확인
       if (date in state) {
+        const isDuplicateId = state[date].some((item) => item.id === rest.id);
+        if (isDuplicateId) return alert("존재하는 영화입니다.");
         // 존재한다면 해당 date 키의 배열에 객체 추가
         state[date].push({ ...rest });
+        alert("저장되었습니다.");
       } else {
         // 존재하지 않는다면 새로운 date 키를 initialState에 추가하고 객체 추가
         state[date] = [{ ...rest }];
+        alert("저장되었습니다.");
+      }
+    },
+    deleteMovie: (
+      state: SavedMovieData,
+      action: PayloadAction<SelectMovieData>
+    ) => {
+      const { date, title } = action.payload;
+
+      if (date in state) {
+        state[date] = state[date].filter((movie) => movie.title !== title);
+        return alert("데이터가 삭제되었습니다.");
+      } else {
+        return alert("데이터가 존재하지 않습니다.");
       }
     },
   },
 });
 
-export const { savedMovie } = savedMovieSlice.actions;
+export const { savedMovie, deleteMovie } = savedMovieSlice.actions;
 export default savedMovieSlice;
