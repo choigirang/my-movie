f78d399bbe50659bc7f3bc880e89fea27a50a5d9
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-07 23:39:57 +0900

    feat: 선택한 데이터 상태값 props => redux

--- src/components/calendar/AddMovie.tsx
+++ src/components/calendar/AddMovie.tsx
@@ -4,23 +4,22 @@ import Alert from "../common/Alert";
 import SearchMovie from "./SearchMovie";
 import SelectMovie from "./SelectMovie";
 import SavedMovieData from "./SavedMovieData";
+import { useAppSelector } from "@/hook/useRedux";
 
-const style = {
-  display: "grid",
-};
+export default function AddMovie() {
+  const selectedDate = useAppSelector((state) => state.movieSlice);
 
-export default function AddMovie({
-  addCalendar,
-}: {
-  addCalendar: Dispatch<SetStateAction<boolean>>;
-}) {
   return (
-    <AlertBg setState={addCalendar}>
-      <Alert setState={addCalendar} customstyle={style}>
-        <SearchMovie />
-        <SelectMovie />
-        <SavedMovieData />
-      </Alert>
-    </AlertBg>
+    <React.Fragment>
+      {selectedDate.date && (
+        <AlertBg>
+          <Alert>
+            <SearchMovie />
+            <SelectMovie />
+            <SavedMovieData />
+          </Alert>
+        </AlertBg>
+      )}
+    </React.Fragment>
   );
 }
