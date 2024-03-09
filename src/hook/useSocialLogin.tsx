b5009e510a9492a186be97be26b8751b4bd8f898
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-03-09 23:22:43 +0900

    chore: edit kakao login url

--- src/hook/useSocialLogin.tsx
+++ src/hook/useSocialLogin.tsx
@@ -3,56 +3,58 @@ import React, { useEffect, useState } from "react";
 import { useAppDispatch } from "./useRedux";
 import { login } from "@/store/modules/userSlice";
 
-const TOKEN_URL = process.env.NEXT_PUBLIC_KAKAO_TOKEN_URL;
-const ACCESS_URL = process.env.NEXT_PUBLIC_KAKAO_ACCESS_URL;
-const ME = process.env.NEXT_PUBLIC_KAKAO_ME;
-
-const headerOption = (token?: string) => {
-  if (token)
-    return {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-      },
-    };
-
-  return {
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  };
-};
+const KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_KEY;
+const KAKAO_REDIRECT = process.env.NEXT_PUBLIC_KAKAO_REDIRECT;
+const KAKAO_TOKEN_URL = process.env.NEXT_PUBLIC_KAKAO_TOKEN_URL;
+const KAKAO_ACCESS_URL = process.env.NEXT_PUBLIC_KAKAO_ACCESS_URL;
 
 export default function useSocialLogin() {
   const dispatch = useAppDispatch();
-
-  const code = new URL(document.location.toString()).searchParams.get("code");
 
   const loginInfo = (id: string, nickname: string, image: string) => {
     dispatch(login({ id, nickname, image }));
   };
 
   const kakaoUserToken = () => {
-    console.log(TOKEN_URL);
-    if (!TOKEN_URL) return console.log("일치하는 URL이 없습니다.");
-    // window.location.href = TOKEN_URL;
+    if (!KAKAO_TOKEN_URL) return console.log("일치하는 URL이 없습니다.");
+    window.location.href = `${KAKAO_TOKEN_URL}&${KAKAO_KEY}&${KAKAO_REDIRECT}`;
   };
 
   const kakaoUserInfoCode = () => {
-    if (!ACCESS_URL || !ME) return console.log("서버 에러입니다.");
+    const params = new URL(document.location.toString()).searchParams;
+    const code = params.get("code");
+    if (!KAKAO_ACCESS_URL) return console.log("서버 에러입니다.");
     axios
-      .post(`${ACCESS_URL}${code}`, {}, headerOption())
+      .post(
+        `${KAKAO_ACCESS_URL}&${KAKAO_KEY}&${KAKAO_REDIRECT}&code=${code}`,
+        {},
+        {
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+          },
+        }
+      )
       .then((res: any) => {
         const { access_token } = res.data;
-        axios.post(ME, {}, headerOption(access_token)).then((res: any) => {
-          const { id, properties } = res.data;
-          loginInfo(id, properties.nickname, properties.thumb_image);
-        });
-      })
-      .catch((Error: any) => {
-        console.log(Error);
+        axios
+          .post(
+            "https://kapi.kakao.com/v2/user/me",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${access_token}`,
+                "Content-type":
+                  "application/x-www-form-urlencoded;charset=utf-8",
+              },
+            }
+          )
+          .then((res: any) => {
+            const { id, properties } = res.data;
+            loginInfo(id, properties.nickname, properties.thumbnail_image);
+            window.location.href = "/";
+          });
       });
   };
 
-  return { kakaoUserToken, kakaoUserInfoCode, code };
+  return { kakaoUserToken, kakaoUserInfoCode };
 }
