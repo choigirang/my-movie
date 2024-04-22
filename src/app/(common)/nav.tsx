e8338c79e30cf13836264a5168932d53dcb96fbe
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-22 23:44:49 +0900

    refactor : 클라이언트 nav

---
+++ src/app/(common)/nav.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import Category from "./category";
+import { useEffect, useState } from "react";
+
+/** 2024/04/04 - 레이아웃 header 컴포넌트 */
+export default function Header() {
+  const [scroll, setScroll] = useState(false);
+
+  /** 스크롤 */
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      setScroll(scrollTop > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className="nav transition-custom"
+      style={{ background: scroll ? "rgba(0,0,0,0.2)" : "none" }}
+    >
+      <div className="container h-full flex justify-between items-center pt-0">
+        <h1 className="text-3xl text-yellow-400 font-bold">
+          <Link href="/" title="Movie" className="font-black">
+            My Movie.
+          </Link>
+        </h1>
+        <Category />
+      </div>
+    </nav>
+  );
+}
