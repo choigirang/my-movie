480acef2e1d9e26d3b0c84d2206c9ec48fbff096
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-23 23:31:10 +0900

    refactor : reduce & combine components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,31 @@
 import { Metadata } from "next";
-import MovieList from "./movieList";
+import { api } from "../api/api";
+import MoreMovie from "./moreMovie";
+import { MovieDetailType } from "@/type/movie";
+import MovieInfo from "./movieInfo";
 
 export const metadata: Metadata = {
   title: "나의 영화 일지",
   description: "영화 기록하기",
 };
 
-export default function Home() {
+async function getMovie() {
+  const res = await api.get("movie/popular", { params: { page: 1 } });
+
+  return res.data;
+}
+
+export default async function Home() {
+  const movies = await getMovie();
+
   return (
     <section className="relative container flex-col justify-between pt-32">
-      <MovieList />
+      <ul className="w-full grid grid-cols-4 place-items-center gap-5 sm:grid-rows-1 sm:grid-cols-1 md:grid-rows-1 md:grid-cols-3">
+        {movies.results.map((movie: MovieDetailType) => (
+          <MovieInfo key={movie.id} {...movie} />
+        ))}
+        <MoreMovie />
+      </ul>
     </section>
   );
 }
