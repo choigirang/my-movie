13a97cc0ac1c15a2d635befaacc2c4831ec0f2f9
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-04-23 23:25:06 +0900

    style

--- src/app/(common)/scrollBtn.tsx
+++ src/app/(common)/scrollBtn.tsx
@@ -6,14 +6,14 @@ import useScroll from "@/hook/useScroll";
 import { ArrowUpCircleIcon } from "@heroicons/react/16/solid";
 
 export default function ScrollBtn() {
-  const scrollTop = useScroll();
+  const { scrollToTop } = useScroll();
 
   return (
     <ArrowUpCircleIcon
-      width={16}
-      height={16}
-      className="fixed flex mx-auto bottom-0 left-1/2 -translate-y-1/2 rotate-90 cursor-pointer transition-custom"
-      onClick={() => scrollTop}
+      width={32}
+      height={32}
+      className="fixed flex mx-auto bottom-[20px] right-[20px] cursor-pointer text-white transition-custom hover:scale-150"
+      onClick={() => scrollToTop()}
     />
   );
 }
